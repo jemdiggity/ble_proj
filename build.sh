adb166000e0d1bfce86dc60ce1cbb8f687e4f660
Jeremy Hale <jeremy.hale@gmail.com>
2017-03-01 16:15:34 +0900

    Add build script

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+bazel build \
+  --crosstool_top=tools/arm_compiler:toolchain \
+  --cpu=armeabi-v7a \
+  --compilation_mode=opt \
+  --verbose_failures \
+  --spawn_strategy=standalone \
+  @nrf5_sdk//...
+
+bazel-ble_proj/external/toolchain_gcc_arm_none_darwin/bin/arm-none-eabi-size \
+  bazel-bin/external/nrf5_sdk/examples_dfu_bootloader_secure_dfu_secure_dfu_ble_s130_pca10028
+
+
