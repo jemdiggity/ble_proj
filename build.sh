89b5287ef55819d4f77bd88c5354978f6eb2a85f
Jeremy Hale <jeremy.hale@gmail.com>
2017-03-20 22:35:42 +0900

    Use rule_os_dependent_http_archive for toolchain and git.bzl, http.bzl rules

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ bazel build \
   --spawn_strategy=standalone \
   @nrf5_sdk//...
 
-bazel-ble_proj/external/toolchain_gcc_arm_none_darwin/bin/arm-none-eabi-size \
+bazel-ble_proj/external/toolchain_gcc_arm_none/bin/arm-none-eabi-size \
   bazel-bin/external/nrf5_sdk/examples_dfu_bootloader_secure_dfu_secure_dfu_ble_s130_pca10028
 
 
